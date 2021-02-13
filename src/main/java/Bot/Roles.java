8ec0f2a7fb1bba65b1ccb52442d5b4a40e170da6
Dominik Ivanoc <dominik.ivanoc@student.tuke.sk>
2021-02-13 15:10:45 +0100

    Update of join/leave output message

--- src/main/java/Bot/Roles.java
+++ src/main/java/Bot/Roles.java
@@ -12,28 +12,28 @@ import java.util.Random;
 
 public class Roles extends ListenerAdapter {
     String[] messages = {
-            "Welcome, [member] joined. You must construct addition pylons!",
-            "Welcome, Never gonna give [member] up! Never gonna let [member] down!",
-            "Hey! Listen! [member] has joined!",
-            "Ha! [member] has joined! You activated my trap card!",
-            "Welcome, [member]. Leave your weapons by the door.",
-            "Welcome, [member]. We hope you brought pizza.",
-            "Brace yourselves. [member] just joined the server.",
-            "[member] just joined. Hide your bananas.",
-            "[member] just arrived. Seems OP - please nerf.",
-            "A [member] has spawned in the server.",
-            "Big [member] showed up!",
-            "Where’s [member]? In the server!",
-            "[member] hopped into the server. Kangaroo!!",
-            "[member] just showed up. Hold my beer.",
-            "Challenger approaching - [member] has appeared!",
-            "It's a bird! It's a plane! Nevermind, it's just [member].",
-            "It's [member]! Praise the sun! [T]/",
-            "Roses are red, violets are blue, [member] joined this server with you",
-            "Hello. Is it [member] you're looking for?",
-            "[member] is here to kick butt and chew bubblegum. And [member] is all out of gum.",
-            "[member] has arrived. Party's over.",
-            "Ready player [member]"
+            "Welcome, **[member]** joined. You must construct addition pylons!",
+            "Welcome, Never gonna give **[member]** up! Never gonna let **[member]** down!",
+            "Hey! Listen! **[member]** has joined!",
+            "Ha! **[member]** has joined! You activated my trap card!",
+            "Welcome, **[member]**. Leave your weapons by the door.",
+            "Welcome, **[member]**. We hope you brought pizza.",
+            "Brace yourselves. **[member]** just joined the server.",
+            "**[member]** just joined. Hide your bananas.",
+            "**[member]** just arrived. Seems OP - please nerf.",
+            "A **[member]** has spawned in the server.",
+            "Big **[member]** showed up!",
+            "Where’s **[member]**? In the server!",
+            "**[member]** hopped into the server. Kangaroo!!",
+            "**[member]** just showed up. Hold my beer.",
+            "Challenger approaching - **[member]** has appeared!",
+            "It's a bird! It's a plane! Nevermind, it's just **[member]**.",
+            "It's **[member]**! Praise the sun! [T]/",
+            "Roses are red, violets are blue, **[member]** joined this server with you",
+            "Hello. Is it **[member]** you're looking for?",
+            "**[member]** is here to kick butt and chew bubblegum. And **[member]** is all out of gum.",
+            "**[member]** has arrived. Party's over.",
+            "Ready player **[member]**"
     };
 
 /*    public void onGuildMemberJoin(GuildMemberJoinEvent event){
@@ -63,7 +63,7 @@ public class Roles extends ListenerAdapter {
         join.setColor(0x0ad13f);
         join.setTitle("Join message:");
         final TextChannel channelPlsChannel = listOfChannel.get(0);
-        final String userWelcomeMessage = (messages[number].replace("[member]",event.getMember().getAsMention()));
+        final String userWelcomeMessage = (messages[number].replace("[member]",event.getMember().getUser().getName()));
         join.setDescription(userWelcomeMessage);
 
         channelPlsChannel.sendMessage(join.build()).queue();
@@ -85,7 +85,7 @@ public class Roles extends ListenerAdapter {
         join.setColor(0xbd2b26);
         join.setTitle("Leave message:");
         final TextChannel channelPlsChannel = listOfChannel.get(0);
-        final String userByeMessage = String.format("Goodbye %s from %s", event.getMember().getUser().getAsTag(),
+        final String userByeMessage = String.format("Goodbye **%s** from %s", event.getMember().getUser().getName(),
                 event.getGuild().getName());
         join.setDescription(userByeMessage);
 
